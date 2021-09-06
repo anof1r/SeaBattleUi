74bbeb07c193023b1c76e7d8d988fe1611aa67b0
Denis <denchik.anofriev@gmail.com>
2021-09-06 13:18:27 +0300

    Authorization service is now working

--- src/app/authorization.service.ts
+++ src/app/authorization.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
   
 })
 export class AuthorizationService {
-  getVal: any;
+  //getVal: any;
 
   constructor(private http: HttpClient) {}
-
+  displayVal = '';
+  getVal(val:string){
+    this.displayVal = val;
+  }
   auth(nick:string){
-    this.http.get<any>('http://localhost:11501/test//eap/'+this.getVal(nick)+',anof1rCOOL').subscribe(message => console.log(message.value))
+    this.getVal(nick);
+    this.http.get<any>('http://localhost:11501/test//eap/'+this.displayVal+',anof1rCOOL').subscribe(message => console.log(message.value))
   }
  
 }
