64c0367444e089f0a20f083196e9cf4adc3ef117
Denis <denchik.anofriev@gmail.com>
2021-09-15 10:33:00 +0300

    Login page error, need to fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AuthorizationService } from './authorization.service';
+import { AppService } from './app.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,13 +10,13 @@ import { AuthorizationService } from './authorization.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SeaBattleUi';
-  greeting = {}
-  constructor(
-    private http: HttpClient,
-    private auth: AuthorizationService
-  )
-  {http.get<any>('http://localhost:11501/test/resource').subscribe(data => this.greeting = data);} 
-  
+  constructor(private app: AppService, private http: HttpClient, private router: Router) {
+      this.app.authenticate(undefined, undefined);
+    }
+    logout() {
+      this.http.post('http://localhost:11501/test/logout', {}).subscribe(() => {
+          this.app.authenticated = false;
+          this.router.navigateByUrl('http://localhost:11501/test/login');
+      })
+    }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,51 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { AppService } from './app.service';
 import { AppComponent } from './app.component';
-import { Routes } from '@angular/router';
+//import { HomeComponent } from './home.component';
+//import { LoginComponent } from './login.component';
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
+} from '@angular/common/http';
 
+//import { Observable } from 'rxjs/Observable';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+@Injectable()
+export class XhrInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const xhr = req.clone({
+      headers: req.headers.set('X-Requested-With', 'XMLHttpRequest')
+    });
+    return next.handle(xhr);
+  }
+}
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AppService, { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
