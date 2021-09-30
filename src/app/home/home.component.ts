aebaa1812dc786191ba4bed2ae93a2f935044556
Denis <denchik.anofriev@gmail.com>
2021-09-30 23:47:17 +0300

    little bit of css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 @Component({
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  clicked = false;
   title = 'Demo';
-  greeting = {};
-  nick: any = {};
+  greeting : any;
+  nick: any;
   avaliable: any = {};
 
 
   constructor(private app: AppService, private http: HttpClient, private router: Router) {
     http.get('http://localhost:4200/rest/resource').subscribe(data => this.greeting = data);
+    http.get('http://localhost:4200/rest/user').subscribe(data => this.nick = data)
   }
-
+  userName(){ 
+   
+  }
   authenticated() { return this.app.authenticated; }
 
   startGame() {
